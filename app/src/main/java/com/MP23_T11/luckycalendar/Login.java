1e03565c27399564ded1decef7692bf962e3a197
Hangyeol-SSeo <dongha313@gmail.com>
2023-05-15 23:49:09 +0900

    feat(core): Adding Login Features
    body:
    Login onclick method - email, goolgle
    
    footer:

--- app/src/main/java/com/MP23_T11/luckycalendar/Login.java
+++ app/src/main/java/com/MP23_T11/luckycalendar/Login.java
@@ -1,14 +1,88 @@
 package com.MP23_T11.luckycalendar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import org.w3c.dom.Text;
 
 public class Login extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    EditText ID;
+    EditText password;
+    TextView create_account;
+    ImageView loginButton;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        ID = (EditText) findViewById(R.id.emailInput);
+        password = (EditText) findViewById(R.id.passwordInput);
+        create_account = (TextView) findViewById(R.id.createAccountButton);
+        loginButton = (ImageView) findViewById(R.id.login);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        create_account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Register.class);
+                startActivity(i);
+                finish();
+            }
+        });
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
+                String email, ps;
+                email = String.valueOf(ID.getText());
+                ps = String.valueOf(password.getText());
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(Login.this, "Enter Email", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (TextUtils.isEmpty(ps)) {
+                    Toast.makeText(Login.this, "Enter Password", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email, ps)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                progressBar.setVisibility(View.GONE);
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Login.this, "Login Successful", Toast.LENGTH_SHORT).show();
+                                    Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                                    startActivity(i);
+                                    finish();
+                                }
+                                else {
+                                    Toast.makeText(Login.this, "Authentication failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
