b9924a034123cbac8ceaba9f905dcda1e342dedd
Hangyeol-SSeo <dongha313@gmail.com>
2023-05-20 16:40:21 +0900

    fix(): Create Account method fix
    body:
    fix
    
    footer:

--- app/src/main/java/com/MP23_T11/luckycalendar/Register.java
+++ app/src/main/java/com/MP23_T11/luckycalendar/Register.java
@@ -4,8 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -20,6 +22,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class Register extends AppCompatActivity {
+    private static final String TAG = "Register";
+
     EditText ID, password, psAagin;
     ImageView signUpButton;
     TextView returnLogin;
@@ -48,6 +52,7 @@ public class Register extends AppCompatActivity {
         psAagin = (EditText) findViewById(R.id.passwordInputAgain);
         signUpButton = (ImageView) findViewById(R.id.signUp);
         returnLogin = (TextView) findViewById(R.id.returnLogin);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         returnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,11 +66,13 @@ public class Register extends AppCompatActivity {
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.d(TAG, "onClick start");
                 progressBar.setVisibility(View.VISIBLE);
                 String email, ps, psA;
                 email = String.valueOf(ID.getText());
                 ps = String.valueOf(password.getText());
                 psA = String.valueOf(psAagin.getText());
+                Log.d(TAG, "getString");
 
                 if (TextUtils.isEmpty(email)) {
                     Toast.makeText(Register.this, "Enter Email", Toast.LENGTH_SHORT).show();
@@ -77,22 +84,30 @@ public class Register extends AppCompatActivity {
                     progressBar.setVisibility(View.GONE);
                     return;
                 }
-                if (TextUtils.equals(ps, psA)) {
+                if (!TextUtils.equals(ps, psA)) {
                     Toast.makeText(Register.this, "Wrong password", Toast.LENGTH_SHORT).show();
                     progressBar.setVisibility(View.GONE);
                     return;
                 }
+                Log.d(TAG, "Defensive coding");
 
                 mAuth.createUserWithEmailAndPassword(email, ps)
                         .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
+                                Log.d(TAG, "onComplete");
                                 progressBar.setVisibility(View.GONE);
                                 if (task.isSuccessful()) {
+                                    Log.d(TAG, "true");
                                     FirebaseUser user = mAuth.getCurrentUser();
+                                    Log.d(TAG, "get Auth");
                                     Toast.makeText(Register.this, "Account created.", Toast.LENGTH_SHORT);
+                                    ID.setText("");
+                                    password.setText("");
+                                    psAagin.setText("");
                                 }
                                 else {
+                                    Log.d(TAG, "false");
                                     Toast.makeText(Register.this, "Authentication failed.", Toast.LENGTH_SHORT);
                                 }
                             }
