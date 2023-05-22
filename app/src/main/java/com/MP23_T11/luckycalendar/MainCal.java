ee296d737e18c7e7f6f662747bd1c819a51ab3e6
thxto-me <hjd0907@gmail.com>
2023-05-22 23:58:13 +0900

    Status Update (May. 22, 2023 23:56)
    Init Main Calendar Page

---
+++ app/src/main/java/com/MP23_T11/luckycalendar/MainCal.java
@@ -0,0 +1,18 @@
+package com.MP23_T11.luckycalendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainCal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_cal);
+    }
+
+    public void addMood(View view) {
+    }
+}
