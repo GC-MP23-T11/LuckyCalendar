9b64dca4ce9db8f169d70f2e37b716f04f7eb904
thxto-me <hjd0907@gmail.com>
2023-05-23 13:39:50 +0900

    feat(UserPage): Init UserPage
    Status Update (May. 22, 2023 23:56)
    Init User page.

---
+++ app/src/main/java/com/MP23_T11/luckycalendar/UserPage.java
@@ -0,0 +1,21 @@
+package com.MP23_T11.luckycalendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class UserPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_page);
+        ImageView ProfilePic = findViewById(R.id.ProfilePic);
+        ProfilePic.setClipToOutline(true);
+    }
+
+    public void modProfilePic(View view) {
+    }
+}
